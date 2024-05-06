dca08feb52c0f78eac1b96f81d3f7f1f71592d4a
m1st-gh <93106448+m1st-gh@users.noreply.github.com>
2024-05-05 21:03:30 -0400

    Fixing

--- starter.py
+++ starter.py
@@ -14,6 +14,6 @@ radio = initialize_serial("/dev/ttyUSB0", 115200)
 
 line = radio.readline()
 print(line)
-if (line == b'START'):
+if (line == b'START\n'):
     subprocess.run(['.venv/bin/python3', 'accmagbar.py'])
     exit(0)
