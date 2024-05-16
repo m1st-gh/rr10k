af4bcca8fcde2712e4296f5017a4020e9cedbcbd
“m1st-“gh <Kushpatel169@gmail.com>
2024-05-16 17:20:05 -0400

    refactor: Update sleep duration in sensor_pkg.py

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pkill -f python3
-source .venv/bin/activate
-timeout 20s python3 calibrate_mag.py
-python3 sensors_pkg.py
--- start_radio.sh
+++ start_radio.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source .venv/bin/activate
-python3 radio_start.py
+timeout 20 python3 calibrate_mag.py
+python3 sensor_pkg.py
